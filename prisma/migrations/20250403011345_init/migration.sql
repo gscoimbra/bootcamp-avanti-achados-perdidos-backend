b9ed6a043eb0be35c4c5b5b2ea786404f2d18f5d
Gabriel <gcoimbrabr03@gmail.com>
2025-04-02 22:19:11 -0300

    Primeiro commit, configurações iniciais e criação da entidades

---
+++ prisma/migrations/20250403011345_init/migration.sql
@@ -0,0 +1,41 @@
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('PERDIDO', 'ENCONTRADO');
+
+-- CreateTable
+CREATE TABLE "Usuario" (
+    "id" SERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+    "telefone" TEXT,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "Usuario_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Categoria" (
+    "id" SERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+
+    CONSTRAINT "Categoria_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Item" (
+    "id" SERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+    "data" TIMESTAMP(3) NOT NULL,
+    "localizacao" TEXT NOT NULL,
+    "contato" TEXT NOT NULL,
+    "foto" TEXT,
+    "status" "Status" NOT NULL,
+    "usuarioId" INTEGER NOT NULL,
+    "categoriaId" INTEGER NOT NULL,
+
+    CONSTRAINT "Item_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Item" ADD CONSTRAINT "Item_usuarioId_fkey" FOREIGN KEY ("usuarioId") REFERENCES "Usuario"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Item" ADD CONSTRAINT "Item_categoriaId_fkey" FOREIGN KEY ("categoriaId") REFERENCES "Categoria"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
